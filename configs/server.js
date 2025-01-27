0330b176dec1d26230ff9294c78bf78bc5613830
Informatica <informatica@alumno.local>
2025-01-27 12:03:19 -0600

    feat(server):se conecto el sercidor a la base de datos

--- configs/server.js
+++ configs/server.js
@@ -4,6 +4,7 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
+import {dbConnection} from "./mongo.js"
 
 const configs = (app) => {
     app.use(cors())
@@ -11,11 +12,21 @@ const configs = (app) => {
     app.use(morgan("dev"))
 }
 
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+    }catch(e){
+        console.log(`DATABASE CONNECTION FAILED: ${e}`)
+        process.exit(1)    
+    }
+}
+
 
 export const initServer = () => {
     const app = express()
     try{
         configs(app)
+        conectarDB()
         app.listen(process.env.PORT)
         console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`)
     }catch(err){
